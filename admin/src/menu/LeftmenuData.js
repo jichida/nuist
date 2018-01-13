1c60220d807bbab309c2554df16c51e0bd4706ea
wangxiaoqing <xiaoqingwang@126.com>
2018-01-13 15:34:04 +0800

    admin 左树图标

--- admin/src/menu/LeftmenuData.js
+++ admin/src/menu/LeftmenuData.js
@@ -3,28 +3,27 @@ import PropTypes from 'prop-types';
 import { translate } from 'admin-on-rest';
 
 import SystemconfigIcon from 'material-ui/svg-icons/action/settings-brightness';//系统设置
-import DeviceIcon from 'material-ui/svg-icons/device/battery-charging-full';//设备
-import DeviceGroupIcon from 'material-ui/svg-icons/device/devices';//设备分组
+import DeviceIcon from 'material-ui/svg-icons/device/devices';//设备
+import DeviceGroupIcon from 'material-ui/svg-icons/action/list';//设备分组
 import UserIcon from 'material-ui/svg-icons/action/account-circle';//用户
-import UserGroupIcon from 'material-ui/svg-icons/action/supervisor-account';//用户分组
-import UserLogIcon from 'material-ui/svg-icons/action/book';//用户登录日志
-import PermissionIcon from 'material-ui/svg-icons/action/info';//权限
-import RealtimeAlarmIcon from 'material-ui/svg-icons/action/alarm';//实时报警
-import RealtimeAlarmRawIcon from 'material-ui/svg-icons/action/alarm';//实时报警
-import HistoryTrackIcon from 'material-ui/svg-icons/action/history';//历史轨迹
-import CanrowDataIcon from 'material-ui/svg-icons/action/timeline';//原始数据
+import ProductIcon from 'material-ui/svg-icons/action/dns';//产品
+import OnlineResearchIcon from 'material-ui/svg-icons/social/poll';//在线调查
+import PermissionIcon from 'material-ui/svg-icons/action/fingerprint';//权限
+import RoleIcon from 'material-ui/svg-icons/action/account-box';//角色
+import RealtimeAlarmRawIcon from 'material-ui/svg-icons/action/alarm';//报警
+import HistoryDeviceIcon from 'material-ui/svg-icons/action/history';//历史设备数据
 
-
+// ic_history
 
 export default [
         { name: 'systemconfig', icon: <SystemconfigIcon /> ,adminonly:true},
-        { name: 'product', icon: <SystemconfigIcon /> ,adminonly:true},
-        { name: 'onlineresearch', icon: <SystemconfigIcon /> ,adminonly:true},
+        { name: 'product', icon: <ProductIcon /> ,adminonly:true},
+        { name: 'onlineresearch', icon: <OnlineResearchIcon /> ,adminonly:true},
         { name: 'device', icon: <DeviceIcon />,adminonly:false },
         { name: 'devicegroup', icon: <DeviceGroupIcon /> ,adminonly:false},
         { name: 'user', icon: <UserIcon /> ,adminonly:true},
-        { name: 'role', icon: <UserGroupIcon /> ,adminonly:true},
+        { name: 'role', icon: <RoleIcon /> ,adminonly:true},
         { name: 'permission', icon: <PermissionIcon /> ,adminonly:true},
         { name: 'realtimealarmraw', icon: <RealtimeAlarmRawIcon /> ,adminonly:false},
-        { name: 'historydevice', icon: <HistoryTrackIcon /> ,adminonly:false},
+        { name: 'historydevice', icon: <HistoryDeviceIcon /> ,adminonly:false},
 ];
