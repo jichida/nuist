6f795106f159208102d125ea62e8b6a15457f105
Jiaowenhui <523947655@qq.com>
2018-02-06 09:28:34 +0800

    补文件

---
+++ app/src/components/investigation/result.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from "../header/page.js";
+import "./style.css";
+import List from "./list.js";
+
+class App extends React.Component {
+
+  	render() {
+	    return (
+	      	<div className="addinvestigationPage result">
+	        	<Header history={this.props.history} />
+	        	<div className="tt"><span>2018-01-17</span><span>共15题</span><span>参与人数963258人</span></div>
+	        	<div className="ll">
+					<ul>
+						<li>
+							<div className="tit">01.新农田水利设施建设以后，以前的水利设施使用情况是？</div>
+							<div className="aslist">
+								<div className="dd"><span>A.和新水利设施共同使用</span></div>
+								<div className="dd"><span>B.和新水利设施共同使用</span></div>
+								<div className="dd"><span>C.和新水利设施共同使用</span></div>
+								<div className="dd"><span>D.和新水利设施共同使用</span></div>
+							</div>
+							<div className="btnlist">
+								<div><span>A</span><span>78523人</span></div>
+								<div className="sel"><span>B</span><span>78523人</span></div>
+								<div><span>C</span><span>78523人</span></div>
+								<div><span>D</span><span>78523人</span></div>
+							</div>
+						</li>
+					</ul>
+	        	</div>
+	      	</div>
+	    );
+  	}
+}
+
+export default App;
