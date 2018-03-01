4f7dd4a139ebd20a4f007c9bf0a8bcaf7eac8209
wangxiaoqing <xiaoqingwang@126.com>
2018-03-01 14:51:54 +0800

    sysconfig

--- deploy/srv/src/handler/common/systemconfig.js
+++ deploy/srv/src/handler/common/systemconfig.js
@@ -7,50 +7,10 @@ exports.getsystemconfig = (actiondata,ctx,callbackfn)=>{
     const dbModel = DBModels.SystemConfigModel;
     dbModel.findOne({},(err,systemconfig)=>{
         if(!err && !!systemconfig){
-            systemconfig = JSON.parse(JSON.stringify(systemconfig));
-            let payload = {};
-            let mappopfields = systemconfig.mappopfields || config.defaultmappopfields;
-            let mapdetailfields = systemconfig.mapdetailfields || config.defaultmapdetailfields;
-            let mappopclusterfields = systemconfig.mappopclusterfields || config.defaultmappopclusterfields;
-            let allfieldslist = [];
-            allfieldslist = _.concat(allfieldslist,mappopfields);
-            _.map(mapdetailfields,(v)=>{
-              allfieldslist = _.concat(allfieldslist,v.fieldslist);
+            callbackfn({
+              cmd:'getsystemconfig_result',
+              systemconfig
             });
-            allfieldslist = _.uniq(allfieldslist);
-            const alname = 'AL_';
-            //还应该包括所有AL开头字母的信息
-            const dbdictModel = DBModels.DataDictModel;
-            dbdictModel.find({
-              $or:[
-                {name:{'$in':allfieldslist}},
-                {name:{'$regex':alname, $options: "i"}}
-              ]},(err,dictlist)=>{
-              //console.log(`dictlist==>${JSON.stringify(dictlist)}`)
-              let mapdict = {};
-              if(!err && dictlist.length > 0){
-                _.map(dictlist,(v)=>{
-                  mapdict[v.name] = {
-                    name:v.name,
-                    showname:v.showname,
-                    unit:v.unit
-                  }
-                });
-              }
-              payload = {
-                mappopfields,
-                mapdetailfields,
-                mappopclusterfields,
-                mapdict
-              };
-              callbackfn({
-                cmd:'getsystemconfig_result',
-                payload
-              });
-
-              config.mapdict = _.merge(config.mapdict,mapdict);
-            });
-
         }
         else{
           callbackfn({
