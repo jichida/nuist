9d02f9476f39fa58d1bf819c0d34bd63d6aa7733
wangxiaoqing <xiaoqingwang@126.com>
2018-03-04 15:02:12 +0800

    产品详情弹框

--- app/src/components/pro/info.js
+++ app/src/components/pro/info.js
@@ -2,31 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Pro1 from "../../img/pro1.png";
 import Close from "../../img/close.png";
+import {
+	set_uiapp,
+} from '../../actions';
+import lodashget from 'lodash.get';
 
 class App extends React.Component {
-
+		onClickPopProductInfo = ()=>{
+			this.props.dispatch(set_uiapp({ispopproductinfo:false}));
+		}
   	render() {
-	    return (
+			const {curproduct} = this.props;
+
+			return (
 	      	<div className="proinfo">
 	      		<div className="wamp"></div>
 	      		<div className="infocontent">
-		        	<div className="tit">温湿度传感器</div>
-		        	<div className="img"><img src={Pro1} /></div>
+		        	<div className="tit">{lodashget(curproduct,'name','')}</div>
+		        	<div className="img"><img src={lodashget(curproduct,'picurl',Pro1)} /></div>
 		        	<div className="info">
-						<p className="t">技术参数</p>
-						<p>测量参数：大气压力</p>
-						<p>测量量程：300~1100 mbar或30~110kpa</p>
-						<p>工作温度：-30～70℃；湿度：10～90%RH</p>
-						<p>储存温度：-30～80℃；湿度：10～90%RH</p>
-						<p>准 确 度：±0.12mbar</p>
-						<p>响应时间：＜1 秒</p>
-						<p>电缆规格：2 米 3 线制（模拟信号）；</p>
-						<p>2 米 4 线制（RS485）（电缆长度可选）</p>
-		        	</div>
-		        	<div className="closediv"><img className="close" src={Close} /></div>
+								<p className="t">{lodashget(curproduct,'brief','')}</p>
+								<div className="imgpicurldetail"><img src={lodashget(curproduct,'picurldetail',Pro1)} /></div>
+							</div>
+		        	<div onClick={this.onClickPopProductInfo} className="closediv"><img className="close" src={Close} /></div>
 	        	</div>
 	      	</div>
 	    );
+
   	}
 }
 
--- app/src/components/pro/list.js
+++ app/src/components/pro/list.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Pro1 from "../../img/pro1.png";
 import lodashmap from 'lodash.map';
+import Info from './info';
+import {
+	set_uiapp,
+} from '../../actions';
 
 class App extends React.Component {
-
+		constructor(props) {  
+	        super(props);  
+	        this.state = {curproduct:null};
+		} 
+    onClickPopProductInfo = (curproduct)=>{
+			this.setState({curproduct});
+      this.props.dispatch(set_uiapp({ispopproductinfo:true}));
+    }
   	render() {
-      const {productlist,products} = this.props;
+      const {productlist,products,ispopproductinfo} = this.props;
 	    return (
 	      	<div className="prolist">
 	        	<ul>
@@ -14,7 +25,7 @@ class App extends React.Component {
                 lodashmap((productlist),(pid)=>{
                   const product = products[pid];
                   if(!!product){
-                    return (<li key={pid}>
+                    return (<li key={pid} onClick={()=>{this.onClickPopProductInfo(product)}}>
       	        			<div>
       	        				<img src={product.picurl} />
       	        				<div className="name">{product.name}</div>
@@ -24,12 +35,13 @@ class App extends React.Component {
                 })
               }
 	        	</ul>
+            { ispopproductinfo && <Info curproduct={this.state.curproduct}/>}
 	      	</div>
 	    );
   	}
 }
 
-const mapStateToProps = ({product:{productlist,products}}) => {
-    return {productlist,products};
+const mapStateToProps = ({product:{productlist,products},app:{ispopproductinfo}}) => {
+    return {productlist,products,ispopproductinfo};
 }
 export default connect(mapStateToProps)(App);
