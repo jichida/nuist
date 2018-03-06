fd9196d62da8760040b6f270ab5c551d47d6812f
wangxiaoqing <xiaoqingwang@126.com>
2018-03-06 17:01:22 +0800

    弹框css

--- app/src/components/map/layerdevice.js
+++ app/src/components/map/layerdevice.js
@@ -1,26 +1,7 @@
-// import React from 'react';
-// import "./layerdevice.css";
 import ImageArrow from '../../img/arrow-right.png';
 import ImageLocation from '../../img/location.png';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className ="weui-dialog">
-//         <div className="weui-dialog__bd">
-//           <p>ID:1522<img src={ImageArrow} /></p>
-//           <p>风力 3级<span>气压 32%</span></p>
-//           <p>风力 3级<span>气压 32%</span></p>
-//           <p>风力 3级<span>气压 32%</span></p>
-//         </div>
-//         <div className="icon-bottom"><img src={ImageLocation} /></div>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-const weui_dialog = `{
+const style_weui_dialog = `
     position: fixed;
     z-index: 5000;
     width: 55%;
@@ -33,54 +14,74 @@ const weui_dialog = `{
     text-align: center;
     border-radius: 0px;
     overflow: hidden;
-}`
+`
 
-const weui_dialog__bd = `{
+const style_weui_dialog__bd = `
     padding:12px;
   	background-color:rgba(0,0,0,0.42);
   	border-radius: 8px;
   	box-shadow: 1px 1px 8px 2px rgba(0,0,0,0.1);
   	margin-bottom:10px;
-}`;
+`;
 
-const weui_dialog__bd_p = `{
+const style_weui_dialog__bd_p = `
     font-size: 13px;
     line-height:20px;
     color: fff;
   	text-align:left;
   	margin: 0px;
-}`;
+`;
 
-const weui_dialog__bd_p_img = `{
+const style_weui_dialog__bd_p_img = `
 	width:20px;
 	height:20px;
 	vertical-align: middle;
   display: inline-flex;
 	float:right;
-}`;
+`;
 
-const weui_dialog__bd_p_span = `{
+const style_weui_dialog__bd_p_span = `
 	float:right;
-}`;
+`;
 
-const icon_bottom_img = `{
+const style_icon_bottom_img = `
 	width:30px;
 	height:30px;
 	vertical-align: middle;
   display: inline-flex;
- }`;
+ `;
 
 const getDeviceLayerHtml = (curdevice)=>{
-  return (`<div style=${weui_dialog}><img src=${ImageLocation} /></div>`);
+  // return (`<div style=${weui_dialog}><img src=${ImageLocation} /></div>`);
   return (
-        `<div style ="weui-dialog">
-          <div class ="weui-dialog__bd">
-            <p>ID:1522<img src=${ImageArrow} /></p>
-            <p>风力 3级<span>气压 32%</span></p>
-            <p>风力 3级<span>气压 32%</span></p>
-            <p>风力 3级<span>气压 32%</span></p>
+        `<div style =${style_weui_dialog}>
+          <div style = ${style_weui_dialog__bd}>
+            <p style = ${style_weui_dialog__bd_p}>
+              ID:1522
+              <img style=${style_weui_dialog__bd_p_img} src=${ImageArrow} />
+            </p>
+            <p style = ${style_weui_dialog__bd_p}>
+              风力 3级
+              <span style=${style_weui_dialog__bd_p_span}>
+                气压 32%
+              </span>
+            </p>
+            <p style = ${style_weui_dialog__bd_p}>
+              风力 3级
+              <span style=${style_weui_dialog__bd_p_span}>
+                气压 32%
+              </span>
+            </p>
+            <p style = ${style_weui_dialog__bd_p}>
+              风力 3级
+              <span style=${style_weui_dialog__bd_p_span}>
+                气压 32%
+              </span>
+            </p>
           </div>
-          <div class="icon-bottom"><img src=${ImageLocation} /></div>
+          <div>
+            <img style=${style_icon_bottom_img} src=${ImageLocation} />
+          </div>
         </div>`
       );
 }
