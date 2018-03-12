439a50e3ddaae998c9c5842a724f45f2c5fc2f27
wangxiaoqing <xiaoqingwang@126.com>
2018-03-12 10:52:57 +0800

    pc端投票

--- pc/src/components/index/investigation.js
+++ pc/src/components/index/investigation.js
@@ -1,22 +1,74 @@
 import React from 'react';
-import "./investigation.css";
+import { connect } from 'react-redux';
+import lodashget from 'lodash.get';
+import lodashmap from 'lodash.map';
+import {setvote_request} from '../../actions';
 
+import "./investigation.css";
 class App extends React.Component {
+  constructor(props) {  
+       super(props); 
+       this.state = {
+         selectedoption:null,
+         voteindex:0
+       };
+  } 
+  componentDidMount(){
+    this.setState({
+      selectedoption:lodashget(this.props,'curvote.selectedoption')
+    });
+  }
+  onClickOption = (selectedoption)=>{
+    this.setState({selectedoption});
+  }
+  onClickAdd = ()=>{
+    const {votelist} = this.props;
+    const voteid = lodashget(this.props,votelist[this.state.voteindex]);
+    const voteresult = lodashget(this.state,'selectedoption');
+    if(!!voteid && !!voteresult){
+      this.props.dispatch(setvote_request({voteid,voteresult}));
+    }
+  }
+  onClickOther = ()=>{
+    let i = this.state.voteindex + 1;
+    i  = i % this.props.votelist.length;
+    this.setState({voteindex:i});
+  }
   render() {
+    const {votelist,votes} = this.props;
+    const curvote = votes[votelist[this.state.voteindex]];
+    if(!curvote){
+      return <div />
+    }
+    const isfilled = lodashget(curvote,'isfilled',false);
+    const {selectedoption} = this.state;
     return (
       <div className="investigation_indexPage">
         <div className="tit">
           <span>在线调查</span>
         </div>
         <div className="investigationli">
-           <div className="t">您对我们的研究项目感兴趣吗？</div>
+           <div className="t">{lodashget(curvote,'name','')}</div>
            <div className="li">
-            <div className="i"><label><input type="checkbox" /><span>很感兴趣</span></label></div>
-            <div className="i"><label><input type="checkbox" /><span>略感兴趣</span></label></div>
-            <div className="i"><label><input type="checkbox" /><span>不感兴趣</span></label></div>
+             {
+               lodashmap(lodashget(curvote,'answeroptions',[]),(option,index)=>{
+                 if(selectedoption === option.optionname){
+                   return (<div key={index} onClick={()=>{
+                     if(!isfilled){
+                       this.onClickOption(option.optionname)
+                     }
+                   }} className="sel"><span>{option.optionname}</span></div>);
+                 }
+                 return (<div  className="i" key={index} onClick={()=>{
+                   if(!isfilled){
+                     this.onClickOption(option.optionname)
+                   }
+                 }}><span>{option.optionname}</span></div>)
+               })
+             }
             <div className="btn">
-              <button>提交</button>
-              <button>查看投票</button>
+              <button onClick={this.onClickAdd.bind(this)}>提交</button>
+              <button onClick={this.onClickOther.bind(this)}>换一批</button>
             </div>
            </div>
         </div>
@@ -25,4 +77,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({vote:{votelist,votes}},props) => {
+
+    return {votelist,votes};
+}
+export default connect(mapStateToProps)(App);
