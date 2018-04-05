bb5234dce912a9b15521c1ba94fcafc9beda498d
wangxiaoqing <xiaoqingwang@126.com>
2018-04-05 21:08:41 +0800

    首页实时数据

--- pc/src/components/index/monitoring.js
+++ pc/src/components/index/monitoring.js
@@ -6,46 +6,48 @@ import Icon from 'antd/lib/icon';
 import {
 	ui_mycar_selcurdevice
 } from '../../actions';
-/*
-问题：
-如果我引入import 'antd/dist/antd.css';则菜单显示正常，但会影响全局的css
-如果我引入单个组件对应的css，则弹框不正常
-如何才能解决这个问题？
-
-我如何才能找到缺失的css，而不引入无关的css?
-*/
 import 'antd/dist/antd.css';
-// import 'antd/lib/menu/style';
-// import 'antd/lib/dropdown/style';
-// import 'antd/lib/button/style';
-// import 'antd/lib/icon/style';
-
 import getMenu from './dropdownselmenu';
 import "./monitoring.css";
 import lodashget from 'lodash.get';
+import lodashmap from 'lodash.map';
 import {getCoureName} from '../../util';
+
+const DeviceInfoDetailList = (props)=>{
+	const {curdevice,devicetype} = props;
+	const {fieldslist_brief,fields} = devicetype[curdevice.devicetype];
+	return (<div className="monitoringli">
+				{
+					lodashmap(fieldslist_brief,(fieldname)=>{
+						const fieldsprops = fields[fieldname];
+						if(!!fieldsprops){
+							let showvalue = lodashget(curdevice,`realtimedata.${fieldname}`);
+							if(fieldname === 'winddirection'){
+								showvalue = getCoureName(lodashget(curdevice,`realtimedata.${fieldname}`));
+							}
+							return (
+								<p key={fieldname}>
+									{`${fieldsprops.showname}`}:{showvalue}{`${lodashget(fieldsprops,'unit','')}`}
+								</p>
+							);
+						}
+					})
+				}
+			  <p>更新时间：{curdevice.updated_at}</p>
+			</div>);
+}
 
 class App extends React.Component {
   onMenuClick(did){
     console.log(did);
 		this.props.dispatch(ui_mycar_selcurdevice(did));
-		// const usersettings = this.props.usersettings;
-		// usersettings.indexdeviceid = did;
-		// this.props.dispatch(saveusersettings_request(usersettings));
   }
   render() {
-    const {curdevice,devicelist,devices} = this.props;
+    const {curdevice,devicelist,devices,devicetype} = this.props;
     if(!curdevice){
       return <div />
     }
     const name = lodashget(curdevice,'name','');
-    const updated_at = lodashget(curdevice,'updated_at','');
-    const degree_point = lodashget(curdevice,'realtimedata.winddirection',0);//指针 win2
-    const windspeed = lodashget(curdevice,'realtimedata.windspeed',0);
-    const temperature = lodashget(curdevice,'realtimedata.temperature',0);
-    const rainfall = lodashget(curdevice,'realtimedata.rainfall',0);
-    const humidity = lodashget(curdevice,'realtimedata.humidity',0);
-    const pressure = lodashget(curdevice,'realtimedata.pressure',0);
     return (
       <div className="monitoring_indexPage">
         <div className="tit">
@@ -60,21 +62,13 @@ class App extends React.Component {
             </Dropdown>
           </span>
         </div>
-        <div className="monitoringli">
-           <p>温度:{temperature}℃</p>
-           <p>湿度:{humidity}%</p>
-           <p>大气压:{pressure}Pa</p>
-           <p>雨量:{rainfall}mm</p>
-           <p>风力:{windspeed}级</p>
-           <p>风向:{getCoureName(degree_point)}</p>
-           <p>更新时间：{updated_at}</p>
-        </div>
+				<DeviceInfoDetailList curdevice={curdevice} devicetype={devicetype} />
       </div>
     );
   }
 }
 
-const mapStateToProps = ({device:{devicelist,devices},userlogin:{usersettings}}) => {
+const mapStateToProps = ({device:{devicelist,devices,devicetype},userlogin:{usersettings}}) => {
 		let curdevice;
 		let curdeviceid = lodashget(usersettings,'indexdeviceid');
 		if(!!curdeviceid){
@@ -85,6 +79,6 @@ const mapStateToProps = ({device:{devicelist,devices},userlogin:{usersettings}})
 				curdevice = devices[devicelist[0]];
 			}
 		}
-    return {curdevice,devicelist,devices,usersettings};
+    return {curdevice,devicelist,devices,usersettings,devicetype};
 }
 export default connect(mapStateToProps)(App);
