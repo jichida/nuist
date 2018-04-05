bd8a5d870e50d4b28d7c0f1c4c9e1133127e402f
wangxiaoqing <xiaoqingwang@126.com>
2018-04-05 10:57:30 +0800

    历史数据

--- app/src/components/history/list.js
+++ app/src/components/history/list.js
@@ -5,42 +5,70 @@ import lodashmap from 'lodash.map';
 import lodashget from 'lodash.get';
 import {getCoureName} from '../../util';
 
+const TitleC = (props)=>{
+	const {fieldslist_brief,fields} = props;
+	return (<div className="tit">
+				{
+					lodashmap(fieldslist_brief,(fieldname)=>{
+						const fieldsprops = fields[fieldname];
+						if(!!fieldsprops){
+							return (<span key={fieldname}>{`${fieldsprops.showname}`}</span>);
+						}
+					})
+				}
+			</div>);
+}
+
+const TitleD = (props)=>{
+	const {curdevice,fieldslist_brief,fields} = props;
+	return (<li>
+				{
+					lodashmap(fieldslist_brief,(fieldname)=>{
+						const fieldsprops = fields[fieldname];
+						if(!!fieldsprops){
+							let showvalue = lodashget(curdevice,`realtimedata.${fieldname}`);
+							if(fieldname === 'winddirection'){
+								showvalue = getCoureName(lodashget(curdevice,`realtimedata.${fieldname}`));
+							}
+							return (<span  key={fieldname}>{showvalue}
+								{`${lodashget(fieldsprops,'unit','')}`}
+							</span>);
+						}
+					})
+				}
+			</li>);
+}
+
+
 class App extends React.Component {
 
   	render() {
-      const {retlist} = this.props;
+      const {retlist,curdevice,devicetype} = this.props;
+			const {fields,fieldslist_brief} = devicetype[curdevice.devicetype];
+
       const ticktimestringlist = lodashget(retlist,'ticktimestring',[]);
 	    return (
 	      	<div className="monitordata">
-	      		<div className="tit">
-	      			<span>风向</span>
-	      			<span>风力</span>
-	      			<span>温度</span>
-	      			<span>湿度</span>
-	      			<span>气压</span>
-	      			<span>雨量</span>
-	      			<span>时间</span>
-	      		</div>
+	      		<TitleC fieldslist_brief={fieldslist_brief} fields={fields} />
 	        	<ul>
               {
                 lodashmap(ticktimestringlist,(vs,index)=>{
-                  const timetickstring = vs;
-                  const temperature = retlist.temperature[index];
-                  const rainfall = retlist.rainfall[index];
-                  const humidity = retlist.humidity[index];
-                  const windspeed = retlist.windspeed[index];
-                  const winddirection = retlist.winddirection[index];
-                  const pressure = retlist.pressure[index];
+                  // const timetickstring = vs;
+                  // const temperature = retlist.temperature[index];
+                  // const rainfall = retlist.rainfall[index];
+                  // const humidity = retlist.humidity[index];
+                  // const windspeed = retlist.windspeed[index];
+                  // const winddirection = retlist.winddirection[index];
+                  // const pressure = retlist.pressure[index];
+									const v = {};
+									lodashmap(fieldslist_brief,(fieldname)=>{
+										v[fieldname] = retlist[fieldname][index];
+									});
+									const curdevice = {
+										realtimedata:v
+									}
                   return (
-                      <li key={index}>
-      	        			<span>{getCoureName(winddirection)}风</span>
-      	        			<span>{windspeed}级</span>
-      	        			<span>{temperature}℃</span>
-      	        			<span>{humidity}%</span>
-      	        			<span>{pressure}Pa</span>
-      	        			<span>{rainfall}mm</span>
-      	        			<span>{timetickstring}</span>
-                    </li>);
+                    <TitleD curdevice={curdevice} fieldslist_brief={fieldslist_brief} fields={fields} />);
                   })
               }
 	        	</ul>
@@ -49,9 +77,9 @@ class App extends React.Component {
   	}
 }
 
-const mapStateToProps = ({historydevice:{historydevices}},props) => {
+const mapStateToProps = ({historydevice:{historydevices},device:{devicetype}},props) => {
     const did = lodashget(props,'curdevice._id');
     const retlist = lodashget(historydevices,`${did}`,[]);
-    return {retlist};
+    return {retlist,devicetype};
 }
 export default connect(mapStateToProps)(App);
