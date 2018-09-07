1fe0aa9f6ea12c99cc5220641544652e64967af4
wangxiaoqing <xiaoqingwang@126.com>
2018-09-08 00:46:57 +0800

    =====

--- pc/src/components/abstract/index.js
+++ pc/src/components/abstract/index.js
@@ -22,29 +22,9 @@ class App extends React.Component {
                     <span className="btn_button">立即登录</span>
                 </div>
             </div>
-            <div className="real_time">
-                <h2 className="title"><img src="images/add.png" alt=""/><span>实时监控</span><em>盘城新居</em></h2>
-                <div className="left_bg">
-                    <p>温度:24℃ </p>
-                    <p> 雨量:531mm</p>
-                    <p>风向:正西 </p>
-                    <p>湿度:38%</p>
-                    <p>风力:7级 </p>
-                    <p>气压:69Pa</p>
-                    <p>更新时间：2018-01-13T<br />14:58:02</p>
-                </div>
-            </div>
-            <div className="real_time">
-                <h2 className="title"><img src="images/add.png" alt=""/><span>在线调查</span></h2>
-                <div className="left_bg">
-                    <div className="label_check">
-                        <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 很棒</label>
-                        <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 一般</label>
-                        <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 中等</label>
-                        <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 差</label>
-                    </div>
-                </div>
-            </div>
+            <Monitoring />
+            <Investigation />
+
         </div>
     )
   }
@@ -55,3 +35,27 @@ const mapStateToProps = ({app:{selectedindex}}) => {
     return {selectedindex};
 }
 export default connect(mapStateToProps)(APP2);
+// 
+// <div className="real_time">
+//     <h2 className="title"><img src="images/add.png" alt=""/><span>实时监控</span><em>盘城新居</em></h2>
+//     <div className="left_bg">
+//         <p>温度:24℃ </p>
+//         <p> 雨量:531mm</p>
+//         <p>风向:正西 </p>
+//         <p>湿度:38%</p>
+//         <p>风力:7级 </p>
+//         <p>气压:69Pa</p>
+//         <p>更新时间：2018-01-13T<br />14:58:02</p>
+//     </div>
+// </div>
+// <div className="real_time">
+//     <h2 className="title"><img src="images/add.png" alt=""/><span>在线调查</span></h2>
+//     <div className="left_bg">
+//         <div className="label_check">
+//             <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 很棒</label>
+//             <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 一般</label>
+//             <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 中等</label>
+//             <label><input type="radio" name="radio1" className="weui-check" checked="checked" />A 差</label>
+//         </div>
+//     </div>
+// </div>
