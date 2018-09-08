c7f732738d17bf237100d4079846c8691aa0f949
wangxiaoqing <xiaoqingwang@126.com>
2018-09-08 09:40:06 +0800

    历史表格

--- pc/src/components/history/charts_history.js
+++ pc/src/components/history/charts_history.js
@@ -41,7 +41,13 @@ class Page extends Component {
     );
   }
 }
-const mapStateToProps = () => {
+const mapStateToProps = ({},props) => {
+  const {fieldslist_brief,ticktimestring,fields,retlist} = props;
+  console.log(fieldslist_brief);
+  console.log(ticktimestring);
+  console.log(fields);
+  console.log(retlist);
+
   const option = {
     tooltip: {
         trigger: 'axis',
@@ -61,7 +67,7 @@ const mapStateToProps = () => {
     xAxis: [
         {
             type: 'category',
-            data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+            data: ticktimestring,
             axisPointer: {
                 type: 'shadow'
             }
@@ -93,13 +99,13 @@ const mapStateToProps = () => {
         {
             name:'降水量',
             type:'bar',
-            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            data:retlist['rainfall']
         },
         {
             name:'平均温度',
             type:'line',
             yAxisIndex: 1,
-            data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            data:retlist['temperature']
         }
     ]
 };
---
+++ pc/src/components/history/charts_history_container.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { connect } from 'react-redux';
+import ChartHistory from "./charts_history";
+import lodashget from 'lodash.get';
+// import lodashmap from 'lodash.map';
+import {
+  gethistorydevicelist_request
+} from '../../actions';
+class App extends React.Component {
+  componentDidMount () {
+    this.onClickQuery(this.props);
+  }
+  onClickQuery = (props)=>{
+    const {periodquery,curdevice,viewtype} = props;
+    const {periodname,starttime,endtime} = periodquery;
+    if(!!curdevice){
+      this.props.dispatch(gethistorydevicelist_request({
+        _id:curdevice._id,
+        periodname,
+        starttime,
+        endtime,
+        fieldslist:viewtype.fieldslist_brief,
+      }));
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+      const oldcurdeviceid = lodashget(this.props,'curdevice._id');
+      const newcurdeviceid = lodashget(nextProps,'curdevice._id');
+      if(oldcurdeviceid !== newcurdeviceid){
+        //chang curdevice
+        this.onClickQuery(nextProps);
+      }
+    }
+
+  	render() {
+      const {curdevice,viewtype,retlist} = this.props;
+      if(!curdevice){
+        return <div />
+      }
+      const ticktimestringlist = lodashget(retlist,'ticktimestring',[]);
+      const {fields,fieldslist_brief} = viewtype;
+			return (
+            <ChartHistory
+              fieldslist_brief={fieldslist_brief}
+              ticktimestring={ticktimestringlist}
+              fields={fields}
+              retlist={retlist}
+                    />
+	    );
+
+  	}
+}
+
+const mapStateToProps = ({historydevice:{periodquery},device:{devices,devicelist,viewtype},historydevice:{historydevices},userlogin:{usersettings}}) => {
+		let curdeviceid = lodashget(usersettings,'indexdeviceid');
+    let curdevice;
+    if(!!curdeviceid){
+      curdevice = devices[curdeviceid];
+    }
+    if(!curdevice){
+      if(devicelist.length > 0){
+        curdevice = devices[devicelist[0]];
+      }
+    }
+    curdeviceid = lodashget(curdevice,'_id');
+    const retlist = lodashget(historydevices,`${curdeviceid}`,[]);
+    return {curdevice,retlist,periodquery,viewtype};
+}
+export default connect(mapStateToProps)(App);
