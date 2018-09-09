2f11d3a31b750ff3d6ee9a9df2854e79218df2e9
wangxiaoqing <xiaoqingwang@126.com>
2018-09-09 18:36:28 +0800

    历史曲线图类型

--- pc/src/components/history/charts_history_humidity.js
+++ pc/src/components/history/charts_history_humidity.js
@@ -55,22 +55,53 @@ const mapStateToProps = (state,props) => {
   // console.log(fields);
   // console.log(retlist);
   const option = {
-      title: {
-          left: 'center',
-          text: curfieldname,
-      },
-      xAxis: {
-          type: 'category',
-          data: ticktimestring
-      },
-      yAxis: {
-          type: 'value'
-      },
-      series: [{
-          data: retlist[curfield],
-          type: 'line'
-      }]
-  };
+    tooltip: {
+       trigger: 'axis',
+       position: function (pt) {
+           return [pt[0], '10%'];
+       }
+   },
+   title: {
+       left: 'center',
+       text: curfieldname,
+   },
+   xAxis: {
+       type: 'category',
+       boundaryGap: false,
+       data: ticktimestring
+   },
+   yAxis: {
+       type: 'value',
+       boundaryGap: [0, '100%']
+   },
+   series: [
+       {
+           name:curfieldname,
+           type:'line',
+           smooth:true,
+           symbol: 'none',
+           sampling: 'average',
+           itemStyle: {
+               normal: {
+                   color: 'rgb(81, 179, 200)'
+               }
+           },
+           areaStyle: {
+               normal: {
+                   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                       offset: 0,
+                       color: 'rgb(119, 151, 164)'
+                   }, {
+                       offset: 1,
+                       color: 'rgb(49, 90, 130)'
+                   }])
+               }
+           },
+           data: retlist[curfield]
+       }
+   ]
+ };
+
  //  const option = {
  //    tooltip: {
  //       trigger: 'axis',
--- pc/src/components/history/charts_history_temperature.js
+++ pc/src/components/history/charts_history_temperature.js
@@ -50,58 +50,24 @@ class Page extends Component {
 }
 const mapStateToProps = (state,props) => {
   const {ticktimestring,retlist,curfield,curfieldname} = props;
-  // console.log(fieldslist_brief);
-  // console.log(ticktimestring);
-  // console.log(fields);
-  // console.log(retlist);
-
   const option = {
-    tooltip: {
-       trigger: 'axis',
-       position: function (pt) {
-           return [pt[0], '10%'];
-       }
-   },
-   title: {
-       left: 'center',
-       text: curfieldname,
-   },
-   xAxis: {
-       type: 'category',
-       boundaryGap: false,
-       data: ticktimestring
-   },
-   yAxis: {
-       type: 'value',
-       boundaryGap: [0, '100%']
-   },
-   series: [
-       {
-           name:curfieldname,
-           type:'line',
-           smooth:true,
-           symbol: 'none',
-           sampling: 'average',
-           itemStyle: {
-               normal: {
-                   color: 'rgb(81, 179, 200)'
-               }
-           },
-           areaStyle: {
-               normal: {
-                   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                       offset: 0,
-                       color: 'rgb(119, 151, 164)'
-                   }, {
-                       offset: 1,
-                       color: 'rgb(49, 90, 130)'
-                   }])
-               }
-           },
-           data: retlist[curfield]
-       }
-   ]
- };
+      title: {
+          left: 'center',
+          text: curfieldname,
+      },
+      xAxis: {
+          type: 'category',
+          data: ticktimestring
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [{
+          data: retlist[curfield],
+          type: 'line'
+      }]
+  };
+
   return {option};
 };
 export default connect(mapStateToProps)(Page);
