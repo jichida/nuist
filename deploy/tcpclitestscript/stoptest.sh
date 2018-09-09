8d32fb993a7662a998132a7a9a4cfa124e60f4f1
wangxiaoqing <xiaoqingwang@126.com>
2018-09-09 22:07:24 +0800

    测试数据

---
+++ deploy/tcpclitestscript/stoptest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pm2 delete tctest_0001.json tctest_0002.json tctest_0003.json tctest_0004.json tctest_0005.json
+pm2 delete tctest_0006.json tctest_0007.json tctest_0008.json tctest_0009.json tctest_000A.json
+pm2 delete tctest_000B.json tctest_000C.json tctest_000D.json tctest_000E.json tctest_000F.json
+pm2 delete tctest_0010.json tctest_0011.json tctest_0012.json tctest_0013.json tctest_0014.json
+pm2 delete tctest_0015.json tctest_0016.json tctest_0017.json tctest_0018.json tctest_0019.json tctest_001A.json
