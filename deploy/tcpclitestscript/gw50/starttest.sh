5abcb77fe7ea7dcf3160583cc420b15ebcdef51d
wangxiaoqing <xiaoqingwang@126.com>
2018-09-12 01:42:07 +0800

    脚本

---
+++ deploy/tcpclitestscript/gw50/starttest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pm2 start tct100.json tct101.json tct102.json tct103.json tct104.json tct105.json tct106.json tct107.json tct108.json tct109.json
+pm2 start tct110.json tct111.json tct112.json tct113.json tct114.json tct115.json tct116.json tct117.json tct118.json tct119.json
+pm2 start tct120.json tct121.json tct122.json tct123.json tct124.json tct125.json tct126.json tct127.json tct128.json tct129.json
+pm2 start tct130.json tct131.json tct132.json tct133.json tct134.json tct135.json tct136.json tct137.json tct138.json tct139.json
+pm2 start tct140.json tct141.json tct142.json tct143.json tct144.json tct145.json tct146.json tct147.json tct148.json tct149.json
