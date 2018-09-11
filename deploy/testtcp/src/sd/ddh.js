0e923aff51c77d9ba06c88628e0b91602ac48c3f
wangxiaoqing <xiaoqingwang@126.com>
2018-09-11 20:20:00 +0800

    模拟程序

--- deploy/testtcp/src/sd/ddh.js
+++ deploy/testtcp/src/sd/ddh.js
@@ -1,6 +1,26 @@
 const debug = require('debug')('testtcp:parse');
 const config = require('../config.js');
 const simulatordata = {
+  "deviceid":{
+    offset:8,
+    length:1,
+    max:200,
+    min:100,
+    gethex:(value)=>{
+      const valuestring = `${value}`;
+      const buf0 = Buffer.allocUnsafe(1);
+      buf0.writeUInt8(value, 0);
+      const hex0 = buf0.toString('hex').toUpperCase();
+      return `${hex0}`;
+    },
+    parsevalue:(hexstring)=>{
+      const buf = Buffer.from(hexstring,'hex');
+      const deviceid =  buf.readUInt8(0);
+      const valuestring = `${deviceid}`;
+      debug(`节点ID为:${valuestring}`);
+      return parseInt(valuestring);
+    }
+  },
   "pressure":{//压力<----PTB210
     offset:parseInt(config.pressure_data_offset),
     length:parseInt(config.pressure_data_length),
@@ -373,12 +393,15 @@ simulatordata.getheader = ({gwid,length,cmd})=>{
   return header;
 }
 
-simulatordata.getbufcmd1 = ({pressure,winddirection,humidity,rainfall,temperature,windspeed})=>{
+simulatordata.getbufcmd1 = ({deviceid,pressure,winddirection,humidity,rainfall,temperature,windspeed})=>{
   let payload = '427E000B7D3100000100000033818600';
   payload+='00DE09E10A660B030BFC002E056606AC';
   payload+='071538306004050607FC030015000000';
   payload+='002B18D0050001299D';
   debug(`payload-->${payload.length}`);
+  const deviceidhex = simulatordata.deviceid.gethex(deviceid);
+  payload = replaceAt(payload,simulatordata.deviceid.offset*2,deviceidhex);
+
   const pressurehex = simulatordata.pressure.gethex(pressure);
   payload = replaceAt(payload,simulatordata.pressure.offset*2,pressurehex);
 
