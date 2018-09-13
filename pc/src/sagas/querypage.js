8760119c25c4f81199c2376d33d445b08ad1911d
wangxiaoqing <xiaoqingwang@126.com>
2018-09-13 21:24:31 +0800

    querypage

--- pc/src/sagas/querypage.js
+++ pc/src/sagas/querypage.js
@@ -3,7 +3,8 @@ import {
   querypage_set_condition,
   querypage_set_condition_sendsrv,
   ui_historydevicequeryselect,
-  gethistorydevicelist_request
+  gethistorydevicelist_request,
+  getrealtimealarmlist_request
 } from '../actions';
 // import moment from 'moment';
 import * as dateMath from '../util/datemath';
@@ -47,6 +48,12 @@ export function* querypageflow(){//仅执行一次
     }
     else if(type === 'alarm'){
       //查询历史报警
+      const {usersettings} = yield select((state)=>{
+        const {usersettings} = state.userlogin;
+        return {usersettings};
+      });
+      const indexdeviceid = lodashget(usersettings,'indexdeviceid','');
+      yield put(getrealtimealarmlist_request({query:{did:indexdeviceid}}));
     }
 
 	// {from: "now-6M", to: "now", display: "Last 6 months", section: 0, active: false}
