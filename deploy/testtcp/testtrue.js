ef8683e3b122c6ef240171e33cffbe6d88aba043
wangxiaoqing <xiaoqingwang@126.com>
2019-04-19 20:54:02 +0800

    ~

--- deploy/testtcp/testtrue.js
+++ deploy/testtcp/testtrue.js
@@ -2,26 +2,29 @@ const net=require('net');
 const debug = require('debug')('testtcp:start');
 const sd = require('./src/sd/sd.js');
 const config = require('./src/config');
-const ip = '127.0.0.1';//'47.97.174.215';//47.97.174.215 //目标ip
-const port = parseInt(config.targetport);//目标端口
+const ip = 'app.nuistiot.com';//'47.97.174.215';//47.97.174.215 //目标ip
+const port = 4000;//parseInt(config.targetport);//目标端口
 let timerInterval;
 let timerTimeout;
 
 const typeofdeviceid = typeof process.env.deviceid;
-debug(`typeofdeviceid->${typeofdeviceid}`)
-debug(`----------->${parseInt(config.deviceid)}->${parseInt(config.senddatainterval)}`);
+// debug(`typeofdeviceid->${typeofdeviceid}`)
+// debug(`----------->${parseInt(config.deviceid)}->${parseInt(config.senddatainterval)}`);
 
 const client= net.connect({port:port,host:ip},()=>{
-  const sendcmd1 = ()=>{
-    const bufhex = sd.getdatahex1(parseInt(config.deviceid));
-    const buf_cmd1 = Buffer.from(bufhex,'hex');
-    client.write(buf_cmd1);
-  }
-
-  sendcmd1();//ping
-  timerInterval = setInterval(()=>{
-    sendcmd1();
-  },parseInt(config.sendpinginterval));
+  const bufhex2 = '594700010000053e50e2d7150037010203427e000b7d3100006c00000033818600002b313733312e36332b333039322e3638020102030405060700010203000102033e17f8070001';//sd.getdatahex2_raw(255,'427e00037d1d00000100000003f10101090011001300230094002100008100007d0007');
+  const buf_cmd2 = Buffer.from(bufhex2,'hex');
+  client.write(buf_cmd2);
+  // const sendcmd1 = ()=>{
+  //   const bufhex = sd.getdatahex1(parseInt(config.deviceid));
+  //   const buf_cmd1 = Buffer.from(bufhex,'hex');
+  //   client.write(buf_cmd1);
+  // }
+  //
+  // sendcmd1();//ping
+  // timerInterval = setInterval(()=>{
+  //   sendcmd1();
+  // },parseInt(config.sendpinginterval));
 });
 
 client.on('data',(data)=>{
@@ -30,9 +33,7 @@ client.on('data',(data)=>{
   //const bufhex1 = '594700010000000650e2280e0037010250427e000b7d3100000100000033818600009c00f103d0066008e903b306f80763091b2f333b04050607f903004800001300371c54080001';
    // const bufhex2 = '594700010000001a50e22eb8003701029f427e000b7d310000010000003381860000c6007c046e06a807180452059006bd07193c613b04050607f6030041000000006e1a97090001';
 
-  const bufhex2 = '594700010000006250e24e7a001f01026b427e00037d1900006c00000003f301020100cf05006900fd05007f00f00900';//sd.getdatahex2_raw(255,'427e00037d1d00000100000003f10101090011001300230094002100008100007d0007');
-  const buf_cmd2 = Buffer.from(bufhex2,'hex');
-  client.write(buf_cmd2);
+
   // timerTimeout = setTimeout(()=>{
   //   const bufhex = sd.getdatahex2(parseInt(config.deviceid));
   //   const buf_cmd2 = Buffer.from(bufhex,'hex');
